993af55aae7938026d70323837f98be73ff37103
dhruv <dhruv>
2021-02-10 20:26:36 +0400

    Rework Stacks

--- Stacks/stacks.go
+++ Stacks/stacks.go
@@ -8,47 +8,48 @@ package Stacks
 
 import (
 	"fmt"
-	"github.com/datdhruvjain/GoDSA/SearchingAlgorithms"
 )
 
-var stack = make([]int, 5)
-var t int = -1
+type IntStack struct {
+	stack []int
+	t     int
+}
 
-func Push(e int) {
-	if t == len(stack)-1 {
+// New creates new stack of integer type
+func (is IntStack) New(size int) *IntStack {
+	return &IntStack{make([]int, size), -1}
+}
+
+// Push Pushes an element onto the stack
+func (is *IntStack) Push(e int) {
+	if is.t == len(is.stack)-1 {
 		fmt.Println("Stack overflow error")
 		return
 	}
 
-	t++
-	stack[t] = e
+	is.t++
+	is.stack[is.t] = e
 }
 
-func Pop() int {
+func (is *IntStack) Pop() int {
 	// here we use < -1 instead of != -1 so that if there is any error, we can recover out of it
-	if t < -1 {
+	if is.t < -1 {
 		// TODo :  add error handling, so that we dont have to return 0
 		fmt.Println("Stack underflow error")
 		return 0
 	}
 
-	temp := stack[t]
-	t--
+	temp := is.stack[is.t]
+	is.t--
 	return temp
 
 	// alternatively, at the cost of being explicit,
 	// return stack[t--]
 }
 
-func Top() int {
-	if t > 0 {
-		return stack[t]
+func (is *IntStack) Top() int {
+	if is.t > 0 {
+		return is.stack[is.t]
 	}
 	return 0
-}
-
-func Search(x int) int {
-	s := make([]int, len(stack)-1)
-	copy(s, stack)
-	return SearchingAlgorithms.BinarySearch(s, x)
 }
--- Stacks/stacks_test.go
+++ Stacks/stacks_test.go
@@ -5,29 +5,8 @@ import (
 	"testing"
 )
 
-func TestPush(t *testing.T) {
-	Push(1)
-	Push(2)
-	Push(3)
-	Push(4)
-	Push(5)
-	if Top() != 5 {
-		t.Log("The Top() function should have returned 5")
-		t.Fail()
-	}
-	Push(6)
-}
-
-func TestPop(t *testing.T) {
-	Pop()
-	Pop()
-	Push(6)
-	Push(7)
-	Push(8)
-	lol := Pop()
-	fmt.Print(lol)
-	if lol != 7 {
-		t.Log("Pop function should have returned 7")
-		t.Fail()
-	}
+var ist IntStack
+func TestNew(t *testing.T) {
+	ist.New(10)
+	fmt.Println(len(ist.stack))
 }
