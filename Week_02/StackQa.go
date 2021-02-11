795dd2040b78a144f03f6125e3309d468516e6f4
dhruv <dhruv>
2021-02-11 14:29:48 +0400

    Completed question 1

--- Week_02/StackQa.go
+++ Week_02/StackQa.go
@@ -1,8 +1,73 @@
 package main
 
- Historical Notes
+import "fmt"
+
+var errFlag bool = false
+var stack *[]int = new([]int)
+var t int = -1
+
+func getSize() int {
+	return t
+}
+
+func isEmpty() bool {
+	if t == -1 {
+		return true
+	}
+	return false
+}
+
+func isFull() bool {
+	if t == getSize()-1 {
+		return true
+	}
+	return false
+}
+
+func top() int {
+	if !isEmpty() {
+		return (*stack)[t]
+	}
+	return -1
+}
+
+func push(e int) {
+	if !isFull() {
+		t++
+		(*stack)[t] = e
+	}
+}
+
+func pop() int {
+	if !isEmpty() {
+		temp := (*stack)[t]
+		t--
+		return temp
+	} else {
+		errFlag = true
+		return -1
+	}
+}
 
 func main() {
-	var s *[]int = new([]int)
-	*s = make([]int, 100)
+	arr := new([]int)
+	fmt.Println("Enter the size of array")
+	arrSize := 0
+	fmt.Scanln(&arrSize)
+
+	*arr = make([]int, arrSize)
+	*stack = make([]int, len(*arr))
+
+	fmt.Println("Enter array elements")
+	for i := 0; i < len(*arr); i++ {
+		fmt.Scanln(&(*arr)[i])
+	}
+
+	for _, e := range *arr {
+		push(e)
+	}
+
+	for i := 0; i < len(*arr); i++ {
+		fmt.Println(pop())
+	}
 }
