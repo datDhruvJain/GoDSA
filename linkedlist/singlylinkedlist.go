736cb5e2a99ed88f01e5ca633f344085f3995767
dhruv <dhruv>
2021-03-12 12:13:23 +0400

    Initial Linked list

--- linkedlist/singlylinkedlist.go
+++ linkedlist/singlylinkedlist.go
@@ -5,13 +5,34 @@ import (
 	"fmt"
 )
 
+var count int = 0
+
 type sllnode struct {
 	data int
-	node *sllnode
+	next *sllnode
 }
 
-func Insert(head *sllnode) (int, error) {
-	if head == nil {
-		return -1, errors.New("Head of the linked list is nil")
+func Insert(head *sllnode, data int, index int) error {
+	if index > count {
+		return errors.New("attempting to insert at index " +
+			"which does not exist for the given linked list")
 	}
+
+	if count == 0 && index == 0 {
+		head = &sllnode{data, nil}
+	}
+	if head == nil {
+		return errors.New("head of the linked list is nil")
+	}
+
+	var temp *sllnode = head
+
+	for (*temp).next != nil {
+		temp = (*temp).next
+	}
+
+	fmt.Printf("Adding new data element with data value: %v\n", data)
+	(*temp).next = &sllnode{data, nil}
+
+	return nil
 }
