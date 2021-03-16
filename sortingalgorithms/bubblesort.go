ca21a22024c2a1a781a02f5ca439b75b6a21e0ac
Dhruv <Dhruv>
2021-03-16 19:29:58 +0400

    Do cool stuff with \r in bubblesort

--- sortingalgorithms/bubblesort.go
+++ sortingalgorithms/bubblesort.go
@@ -1,5 +1,10 @@
 package sortingalgorithms
 
+import (
+	"fmt"
+	"time"
+)
+
 // TODo : Write good documentation for this
 
 // Remember : Arrays are passed by reference, not by value.
@@ -9,7 +14,17 @@ func BubbleSort(arr []int) {
 			// len - i - 1, because the arr[len-1]th element has no arr[j+1], which we are using
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				fmt.Printf("%v\r", arr)
+				time.Sleep(1000 * time.Millisecond)
 			}
 		}
 	}
+	fmt.Println(arr)
 }
+
+/*
+func main() {
+	var arr []int = []int{5, 3, 1, 2, 6, 7, 9, 4}
+	BubbleSort(arr)
+}
+*/
